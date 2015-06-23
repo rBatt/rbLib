cdb9cee399a22fb8bce55b78bd6686abeaddfb0f
rbatt <battrd@gmail.com>
2015-06-23 18:18:14 -0400

    update package documentation for sync mirror

--- R/rbLib-package.r
+++ R/rbLib-package.r
@@ -9,6 +9,10 @@
 #' \code{\link{run}} to run script on server
 #' \code{\link{prp}} to push to a server, then run script there, then pull from it
 #'
+#' @section Generic Syncing:
+#' \code{\link{sync}} similar to a \code{\link{push}} followed by a \code{\link{pull}}, except more generic in that it doesn't assume one location is remote, the two locations don't need to have same path (necessary if both are local), and includes option to exclude certain directories/ files. Never deletes files.
+#' \code{\link{mirror}} like \code{\link{sync}}, but unidirectional and deletes files at the destination that are not present in the source.
+#'
 #' @note These scripts were written specifically for the case where the local system is running OSX, and the remote is Linux. For the ssh to work, you have to already have a key so that you don't need to enter a password. See \href{http://www.thegeekstuff.com/2008/11/3-steps-to-perform-ssh-login-without-password-using-ssh-keygen-ssh-copy-id/}{HERE} for how to do that.
 #'
 #' @docType package

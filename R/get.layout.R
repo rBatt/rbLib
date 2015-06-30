6d40e44d4fd16c70903a890f0524248cd0711531
rbatt <battrd@gmail.com>
2015-06-30 19:01:53 -0400

    fix ascii, commenting in examples, and require

--- R/get.layout.R
+++ R/get.layout.R
@@ -8,7 +8,7 @@
 #' @param return_r logical. if FALSE (default) a layout matrix is returned; otherwise returns a vector specifying same relative widths as \code{ratios}, but as integers.
 #'
 #' @details 
-#' Returns a matrix that can be supplied to \code{\link{layout}} to specify the arrangement and size of figure panels. Current version assumes that all rows will have equal height, but that columns may have variable width; however, \code{get.layout} can be called twice to generate the matrix specifying variable widths and heights (see examples), so long as all panels in a given row have the same height, and all panels in a given column have the same width. Also note that the ratios do not need to be integers – this feature makes it easy and fast to adjust figure layout.
+#' Returns a matrix that can be supplied to \code{\link{layout}} to specify the arrangement and size of figure panels. Current version assumes that all rows will have equal height, but that columns may have variable width; however, \code{get.layout} can be called twice to generate the matrix specifying variable widths and heights (see examples), so long as all panels in a given row have the same height, and all panels in a given column have the same width. Also note that the ratios do not need to be integers - this feature makes it easy and fast to adjust figure layout.
 #'
 #' @seealso See \code{\link{rbLib-package}} for package overview.
 #'
@@ -18,14 +18,14 @@
 #'
 #' # calling get.layout twice can allow you to specify
 #' # variable widths and variable heights
-#' # c.h <- get.layout(nr=3, nc=3, ratios=c(4.2, 3, 8.1)) # from previous example
-#' # r.w <- get.layout(3, 3, c(1.5, 2, 2.5), TRUE) # integers of relative widths
-#' # layout.mat <- matrix(rep(c(c.h), rep(r.w, ncol(c.h))), ncol=ncol(c.h))
-#' # layout(layout.mat)
-#' # for(i in 1:9){plot(rnorm(5))}
+#' c.h <- get.layout(nr=3, nc=3, ratios=c(4.2, 3, 8.1)) # from previous example
+#' r.w <- get.layout(3, 3, c(1.5, 2, 2.5), TRUE) # integers of relative widths
+#' layout.mat <- matrix(rep(c(c.h), rep(r.w, ncol(c.h))), ncol=ncol(c.h))
+#' layout(layout.mat)
+#' for(i in 1:9){plot(rnorm(5))}
 
 get.layout <- function(nr, nc, ratios, return_r=FALSE){
-	require(numbers)
+	# require(numbers)
 	stopifnot(nc>1)
 	stopifnot(length(ratios)==nc)
 	

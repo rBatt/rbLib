1d510fc55061401184b3ee2016b0a82b5656479f
rbatt <battrd@gmail.com>
2015-08-03 19:54:25 -0400

    add option for local and remote paths to differ

--- R/prp.R
+++ R/prp.R
@@ -2,13 +2,14 @@
 #'
 #' @description \code{push} makes a remote copy of a new or updated local file(s)
 #'
-#' @param path Character string for path to be pushed, or path containing file to be pushed
+#' @param path Character string for path to be pushed, or path containing file to be pushed. If \code{path2} is specified, \code{path} refers only to local source path.
 #' @param remoteName Character string for remote server;
 #' @param fileName Character string of file within \code{path} to be pushed. If \code{""}, all files in \code{path} are pushed
 #'@param verbose logical. print extra output.
+#'@param path2 remote destination path; if blank, \code{path2} is the remote equivalent of \code{path}
 #'
 #' @details
-#'Works via ssh and rsync. Can push all files in a folder, or a specific file in the specified path. The only files that are altered remotely are those that a newer locally (or only present locally). Will not delete a remote file if it isn't already present locally, and will not overwrite a remote file if the local version is older (or absent). Does not change local files. Assumes local and remote file paths are identical.
+#'Works via ssh and rsync. Can push all files in a folder, or a specific file in the specified path. The only files that are altered remotely are those that a newer locally (or only present locally). Will not delete a remote file if it isn't already present locally, and will not overwrite a remote file if the local version is older (or absent). Does not change local files. Assumes local and remote file paths are identical unless \code{path2} is specified.
 #'
 #' @seealso \code{\link{pull}} for the opposite process, \code{\link{run}} to run script remotely, and \code{\link{prp}} to push run pull. See \code{\link{rbLib-package}} for overview.
 #'
@@ -19,9 +20,12 @@
 #' remoteName <- "ryanb@@amphiprion.deenr.rutgers.edu"
 #' (push(path, remoteName, fileName=scriptName))
 #' }
-push <- function(path, remoteName, fileName="", verbose=FALSE){
+push <- function(path, remoteName, fileName="", verbose=FALSE, path2){
 	# from local, push script to remote
-	path2 <- gsub("&", "\\\\&", path, perl=TRUE)
+
+	if(missing(path2)){
+		path2 <- gsub("&", "\\\\&", path, perl=TRUE)
+	}
 	
 	rsync.cmd <- "rsync -azuP --stats "
 	
@@ -90,10 +94,11 @@ run <- function(scriptName, path, remoteName, verbose=FALSE, debugMode=FALSE){
 #'
 #' @description \code{pull} makes a local copy of a new or updated remote file(s)
 #'
-#' @param path Character string for path to be pulled, or path containing file to be pulled
+#' @param path Character string for path to be pulled, or path containing file to be pulled. If \code{path2} is specified, \code{path} refers only to the local destination.
 #' @param remoteName Character string for remote server in username@@place.edu format. See \code{run.remote} in the package \code{ssh.utils}
 #' @param fileName Character string of file within \code{path} to be pulled. If \code{""}, all files in \code{path} are pulled
 #' @param verbose logical. print extra output.
+#'@param path2 remote source path; if blank, \code{path2} is the remote equivalent of \code{path}
 #'
 #' @seealso \code{\link{push}} for the opposite process, \code{\link{run}} to run script remotely, and \code{\link{prp}} to push run pull. See \code{\link{rbLib-package}} for overview.
 #'
@@ -104,10 +109,12 @@ run <- function(scriptName, path, remoteName, verbose=FALSE, debugMode=FALSE){
 #' remoteName <- "ryanb@@amphiprion.deenr.rutgers.edu"
 #' (pull(path, remoteName, fileName=""))
 #' }
-pull <- function(path, remoteName, fileName="", verbose=FALSE){
+pull <- function(path, remoteName, fileName="", verbose=FALSE, path2){
 	ss1 <- fileSnapshot(path, full.names=TRUE)
 	# from local, pull files updated by script
-	path2 <- gsub("&", "\\\\&", path, perl=TRUE)
+	if(missing(path2)){
+		path2 <- gsub("&", "\\\\&", path, perl=TRUE)
+	}
 	rsync.cmd <- "rsync -azuP --stats "
 	
 	pull.sh <- paste0(
@@ -144,6 +151,7 @@ pull <- function(path, remoteName, fileName="", verbose=FALSE){
 #' @param remoteName Character string for remote server in username@@place.edu format. See \code{run.remote} in the package \code{ssh.utils}
 #' @param verbose logical. print extra output.
 #' @param debugMode logical. whether the run should load a file called '.RData' present in the directory, and if it should save a file (image) by the same name on exit.
+#'@param path2 remote path; if blank, \code{path2} is the remote equivalent of \code{path}
 #'
 #' @seealso \code{\link{pull}} and \code{\link{push}} to sync files, and \code{\link{run}} to run script remotely.  See \code{\link{rbLib-package}} for overview.
 #'
@@ -154,13 +162,13 @@ pull <- function(path, remoteName, fileName="", verbose=FALSE){
 #' remoteName <- "ryanb@@amphiprion.deenr.rutgers.edu"
 #' prp(path, scriptName, remoteName, verbose=TRUE)
 #' }
-prp <- function(path, scriptName, remoteName, verbose=FALSE, debugMode=FALSE){	
+prp <- function(path, scriptName, remoteName, verbose=FALSE, debugMode=FALSE, path2){	
 	if(verbose){cat("pushing\n")}
-	push(path, remoteName, fileName=scriptName, verbose=verbose)
-	
+	push(path, remoteName, fileName=scriptName, verbose=verbose, path2=path2)
+
 	if(verbose){cat("running\n")}
-	run(scriptName, path, remoteName, verbose=verbose, debugMode=debugMode)
+	run(scriptName, path=path2, remoteName, verbose=verbose, debugMode=debugMode)
 	
 	if(verbose){cat("pulling\n")}
-	pull(path, remoteName, fileName="", verbose=verbose)
+	pull(path, remoteName, fileName="", verbose=verbose, path2=path2)
 }
